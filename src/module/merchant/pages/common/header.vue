2e912008421b69fbdb38acc3afff6e47212054e3
myxingke <myxingke@126.com>
2017-05-24 14:38:47 +0800

    分离头部

---
+++ src/module/merchant/pages/common/header.vue
@@ -0,0 +1,37 @@
+<style src="../../assets/style/common/manage.less" lang="less" scoped></style>
+<template>
+    <div class="header box-shadow">
+        <h1 class="logo"><a href="/">代码兔微信公众平台欢迎你</a></h1>
+        <div class="top-panel">
+            <span class="divide-icon pull-left">|</span>
+            <a class="user-info" href="#/user">
+                <img
+                    src="https://o1aa0tynl.qnssl.com/3ce66694636c4db500e0607cec87791e010fa3d8c2d5c609832af3979bb5e768?imageMogr2/crop/!87.99999999999999x88a5.000000000000013a0/thumbnail/120x120">
+                <span class="simple-text user-name">baide</span>
+            </a>
+            <span class="divide-icon pull-left">|</span>
+            <a href="/app/logout" class="logout-btn">退出</a>
+            <div class="clearfix"></div>
+        </div>
+        <div class="feedback-panel">
+            <i class="message">
+                <div class="notification"></div>
+            </i>
+        </div>
+        <div class="nav">
+            <ul class="list-inline">
+                <li><a href="#/myclass" class="active">财务</a></li>
+                <li><a href="#/message">工单</a></li>
+                <li><a href="#/score">帮助</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+
+        }
+    }
+</script>
--- src/module/merchant/pages/common/manage.vue
+++ src/module/merchant/pages/common/manage.vue
@@ -2,32 +2,9 @@
 <style src="../../assets/style/common/iconfont.css" lang="less" scoped></style>
 <template>
     <div class="fixedDiv">
-        <div class="header box-shadow">
-            <h1 class="logo"><a href="/">班级优化大师-抓住孩子的每一课闪光点</a></h1>
-            <div class="top-panel">
-                <span class="divide-icon pull-left">|</span>
-                <a class="user-info" href="#/user">
-                    <img
-                        src="https://o1aa0tynl.qnssl.com/3ce66694636c4db500e0607cec87791e010fa3d8c2d5c609832af3979bb5e768?imageMogr2/crop/!87.99999999999999x88a5.000000000000013a0/thumbnail/120x120">
-                    <span class="simple-text user-name">baide</span>
-                </a>
-                <span class="divide-icon pull-left">|</span>
-                <a href="/app/logout" class="logout-btn">退出</a>
-                <div class="clearfix"></div>
-            </div>
-            <div class="feedback-panel">
-                <i class="message">
-                    <div class="notification"></div>
-                </i>
-            </div>
-            <div class="nav">
-                <ul class="list-inline">
-                    <li><a href="#/myclass" class="active">财务</a></li>
-                    <li><a href="#/message">工单</a></li>
-                    <li><a href="#/score">帮助</a></li>
-                </ul>
-            </div>
-        </div>
+        <!--头部组件-->
+        <Header></Header>
+        <!--头部组件结束-->
 
         <div class="contentDiv">
             <div class="content">
@@ -150,6 +127,7 @@
 
 <script>
     import CommonMain from "./main.vue"
+    import Header from "./header.vue"
 
     export default {
         data () {
@@ -158,7 +136,8 @@
             }
         },
         components: {
-            CommonMain
+            CommonMain,
+            Header
         },
     }
 </script>
