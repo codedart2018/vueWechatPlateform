74d71e2c614895edb871cb14396ae75fa196fa9e
myxingke <myxingke@126.com>
2017-05-27 18:19:36 +0800

    文章列表

---
+++ src/module/manage/pages/article/edit.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div>this is template body</div>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+
+        }
+    }
+</script>
