99c45b1f716366c99fc46de494df3ccb29d19e81
myxingke <myxingke@126.com>
2017-06-07 12:01:26 +0800

    自定义菜单

--- src/module/merchant/pages/wechat/menu.vue
+++ src/module/merchant/pages/wechat/menu.vue
@@ -1,47 +1,47 @@
 <style src="../../assets/style/wechat/menu.less" lang="less" scoped></style>
 <template>
     <div>
-        <div class="menu_setting_box js_menuBox dn" style="display: flex;">
+        <div style="display: flex;">
             <!--手机模块-->
-            <div class="menu_setting_area">
-                <div class="menu_preview_area">
-                    <div class="mobile_menu_preview">
-                        <div class="mobile_hd tc">
+            <div class="menu-setting-area">
+                <div class="preview-area">
+                    <div class="mobile-menu-preview">
+                        <div class="mobile-hd">
                             {{ publicName }}
                         </div>
-                        <div class="mobile_bd">
-                            <ul class="pre_menu_list grid_line ui-sortable ui-sortable-disabled no_menu" style="z-index:3;" id="menuList">
-                                <li v-for="(btn,index) in menu.button" :key="btn.id" class="jsMenu pre_menu_item grid_item jslevel1 size1of3 ui-sortable ui-sortable-disabled">
-                                    <a href="javascript:void(0);" @click="menu_selected(btn.name,index)" :class="[{pre_menu_link: index===activeMenuIndex && activeMenuType()==1}]" draggable="false">
-                                        <i class="icon_menu_dot js_icon_menu_dot dn"></i>
-                                        <i class="icon20_common sort_gray"></i>
+                        <div class="mobile-bd">
+                            <ul class="pre-menu-list no_menu" style="z-index:3;">
+                                <li v-for="(btn,index) in menu.button" :key="btn.id" class="pre-menu-item grid-item size1of3">
+                                    <a href="javascript:void(0);" @click="menuSelected(btn.name,index)" :class="[{'pre-menu-link': index===activeMenuIndex && activeMenuType()==1}]" draggable="false">
+                                        <i class="icon-menu-dot dn"></i>
+                                        <i class="icon-common sort-gray"></i>
                                         <span>{{ btn.name }}</span>
                                     </a>
-                                    <div class="sub_pre_menu_box" v-if="index===activeMenuIndex">
-                                        <ul class="sub_pre_menu_list">
+                                    <div class="sub-pre-menu-box" v-if="index===activeMenuIndex">
+                                        <ul class="sub-pre-menu-list" style="border: 1px solid #d0d0d0;">
                                             <li v-for="(sub,index) in btn.sub_button" :key="sub.id" :class="[{current: index===activeMenuItemIndex && activeMenuType()==2}]">
-                                                <a href="javascript:void(0);"  @click="menu_item_selected(sub.name,index)" draggable="false">
-                                                    <span class="sub_pre_menu_inner">
-                                                        <i class="icon20_common sort_gray"></i>
+                                                <a href="javascript:void(0);"  @click="menuItemSelected(sub.name,index)" draggable="false">
+                                                    <span class="sub-pre-menu-inner">
+                                                        <i class="icon-common sort-gray"></i>
                                                         <span>{{ sub.name }}</span>
                                                     </span>
                                                 </a>
                                             </li>
                                             <li v-if="isSet(menu.button[activeMenuIndex].sub_button)&&btn.sub_button.length>=0&&btn.sub_button.length < 5">
-                                                <a href="javascript:void(0);"  title="最多添加5个子菜单" @click="menu_item_add()" draggable="false">
-                                                    <span class="sub_pre_menu_inner">
-                                                        <i class="icon14_menu_add"></i>
+                                                <a href="javascript:void(0);"  title="最多添加5个子菜单" @click="menuItemAdd" draggable="false">
+                                                    <span class="sub-pre-menu-inner">
+                                                        <i class="icon-menu-add"></i>
                                                     </span>
                                                 </a>
                                             </li>
                                         </ul>
-                                        <i class="arrow arrow_out"></i>
-                                        <i class="arrow arrow_in"></i>
+                                        <i class="arrow arrow-out"></i>
+                                        <i class="arrow arrow-in"></i>
                                     </div>
                                 </li>
-                                <li class="pre_menu_item grid_item no_extra size1of1">
-                                    <a href="javascript:void(0);" v-if="isSet(menu.button)?(menu.button.length>=0&&menu.button.length<3):false" @click="menu_add()" :class="[{pre_menu_link: activeMenuIndex===''}]" title="最多添加3个一级菜单" draggable="false">
-                                        <i class="icon14_menu_add"></i>
+                                <li class="pre-menu-item grid-item no-extra size1of1">
+                                    <a href="javascript:void(0);" v-if="isSet(menu.button)?(menu.button.length>=0&&menu.button.length<3):false" @click="menuAdd" :class="[{'pre-menu-link': activeMenuIndex===''}]" title="最多添加3个一级菜单" draggable="false">
+                                        <i class="icon-menu-add"></i>
                                         <span>添加菜单</span>
                                     </a>
                                 </li>
@@ -57,13 +57,13 @@
                     <div class="menu-title-bar">
                         <h4 class="title">菜单名称</h4>
                         <div class="delete">
-                            <a href="javascript:void(0);" @click="menu_del()" v-if="showDelBtnType===1">删除菜单</a>
-                            <a href="javascript:void(0);" @click="menu_item_del()" v-if="showDelBtnType===2">删除子菜单</a>
+                            <a href="javascript:void(0);" @click="menuDel" v-if="showDelBtnType===1">删除菜单</a>
+                            <a href="javascript:void(0);" @click="menuItemDel" v-if="showDelBtnType===2">删除子菜单</a>
                         </div>
                     </div>
-                    <div class="menu-form-box">
-                        <div style="display: block;" class="msg-sender-tips">已为“菜单名称”添加了5个子菜单，无法设置其他内容。</div>
-                    </div>
+                    <!--<div class="menu-form-box">-->
+                        <!--<div style="display: block;" class="msg-sender-tips">已为“菜单名称”添加了5个子菜单，无法设置其他内容。</div>-->
+                    <!--</div>-->
                     <div class="form-group">
                         <div class="form-item">
                             <label class="form-item-label" style="width: 80px;">菜单名称</label>
@@ -77,11 +77,11 @@
                             </div>
                         </div>
 
-                        <div class="form-item">
+                        <div class="form-item" v-show="((activeMenuIndex > 0 || activeMenuIndex === 0) && (menu.button[activeMenuIndex].sub_button.length == 0)) || (activeMenuItemIndex > 0 || activeMenuItemIndex === 0) " >
                             <label class="form-item-label" style="width: 80px;">菜单内容</label>
                             <div class="ivu-form-item-content" style="margin-left: 80px;">
                                 <div class="ivu-radio-group">
-                                    <Radio-group v-model="showMenuContentType" @on-change="radio_label_selected">
+                                    <Radio-group v-model="showMenuContentType" @on-change="radioLabelSelected">
                                         <Radio label="1">发送消息</Radio>
                                         <Radio label="2">跳转网页</Radio>
                                         <Radio label="3">跳转小程序</Radio>
@@ -189,7 +189,8 @@
                                 <div class="ivu-form-item-content" style="margin-left: 80px;">
                                     <div class="ivu-input-wrapper ivu-input-type">
                                         <i class="ivu-icon ivu-icon-load-c ivu-load-loop ivu-input-icon ivu-input-icon-validate"></i>
-                                        <input type="text" placeholder="请填写链接地址" class="ivu-input" style="width: 250px">
+                                        <input type="text" placeholder="请填写链接地址" class="ivu-input" v-if="activeMenuType() == 1" v-model="menu.button[activeMenuIndex].url" >
+                                        <input type="text" placeholder="请填写链接地址" class="ivu-input" v-if="activeMenuType() == 2"  v-model="menu.button[activeMenuIndex].sub_button[activeMenuItemIndex].url" >
                                         <p>从公众号图文消息中选择</p>
                                     </div>
                                 </div>
@@ -202,7 +203,6 @@
                         </div>
                     </div>
                 </div>
-
 
                 <div class="editor-inner-notice" v-show="menu.button.length == 0 || activeMenuIndex===''">
                     请点击左侧菜单进行编辑操作
@@ -219,229 +219,6 @@
     </div>
 </template>
 
-<style>
-    .menu_setting_area {
-        /*margin: 14px 30px 0;*/
-        margin: 0
-    }
-    .menu_preview_area {
-        float: left;
-        margin-right: 12px;
-        position: relative;
-    }
-    .mobile_menu_preview {
-        position: relative;
-        width: 317px;
-        height: 580px;
-        background: transparent url(https://res.wx.qq.com/mpres/htmledition/images/bg/bg_mobile_head_default2968da.png) no-repeat 0 0;
-        background-position: 0 0;
-        border: 1px solid #e7e7eb;
-    }
-    .mobile_menu_preview .mobile_hd {
-        color: #fff;
-        text-align: center;
-        padding-top: 30px;
-        font-size: 15px;
-        width: auto;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        word-wrap: normal;
-        margin: 0 30px;
-    }
-    .menu_preview_area .pre_menu_list {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        border-top: 1px solid #e7e7eb;
-        background: transparent url(https://res.wx.qq.com/mpres/htmledition/images/bg/bg_mobile_foot_default2968da.png) no-repeat 0 0;
-        background-position: 0 0;
-        background-repeat: no-repeat;
-        padding-left: 43px;
-    }
-    .menu_preview_area .pre_menu_item {
-        line-height: 50px;
-    }
-    .pre_menu_item {
-        position: relative;
-        float: left;
-        line-height: 38px;
-        text-align: center;
-    }
-    .size1of3 {
-        width: 33.33%;
-    }
-    .grid_item {
-        float: left;
-    }
-    .pre_menu_item a {
-        display: block;
-        width: auto;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        word-wrap: normal;
-        color: #616161;
-        text-decoration: none;
-    }
-    .pre_menu_item .icon_menu_dot {
-        background: url(https://res.wx.qq.com/mpres/zh_CN/htmledition/comm_htmledition/style/page/menu/index_z354723.png) 0 -36px no-repeat;
-        width: 7px;
-        height: 7px;
-        vertical-align: middle;
-        display: inline-block;
-        margin-right: 2px;
-        margin-top: -2px;
-    }
-    .dn {
-        display: none;
-    }
-    .menu_preview_area .pre_menu_list .sort_gray {
-        margin-top: -4px;
-    }
-    .menu_preview_area .pre_menu_list .sort_gray {
-        display: none;
-    }
-    .icon20_common.sort_gray {
-        background: url(https://res.wx.qq.com/mpres/zh_CN/htmledition/comm_htmledition/style/base/base_z3560fc.png) 0 -4779px no-repeat;
-    }
-    .icon20_common {
-        width: 20px;
-        height: 20px;
-        vertical-align: middle;
-        display: inline-block;
-        line-height: 100px;
-        overflow: hidden;
-    }
-    .grid_line .no_extra.grid_item {
-        float: none;
-        width: auto;
-        overflow: hidden;
-    }
-    .menu_preview_area .pre_menu_item {
-        line-height: 50px;
-    }
-    .pre_menu_item {
-        position: relative;
-        float: left;
-        line-height: 38px;
-        text-align: center;
-    }
-    .size1of1 {
-        width: 100%;
-    }
-    .grid_item {
-        float: left;
-    }
-    .menu_preview_area .pre_menu_list.no_menu .pre_menu_item .pre_menu_link {
-        border: 1px solid #44b549;
-        line-height: 48px;
-        background-color: #fff;
-        color: #44b549;
-    }
-    .menu_preview_area .pre_menu_list.no_menu .icon14_menu_add {
-        background: url(https://res.wx.qq.com/mpres/zh_CN/htmledition/comm_htmledition/style/page/menu/index_z354723.png) 0 -18px no-repeat;
-    }
-    .menu_preview_area .icon14_menu_add {
-        background: url(https://res.wx.qq.com/mpres/zh_CN/htmledition/comm_htmledition/style/page/menu/index_z354723.png) 0 0 no-repeat;
-        width: 14px;
-        height: 14px;
-        vertical-align: middle;
-        display: inline-block;
-        margin-top: -2px;
-    }
-
-
-
-    /*添加菜单样式*/
-    .menu_preview_area .sub_pre_menu_box {
-        bottom: 60px;
-        background-color: #fafafa;
-        border-top-width: 0;
-    }
-    .sub_pre_menu_box {
-        position: absolute;
-        bottom: 50px;
-        left: 0;
-        width: 100%;
-        border: 1px solid #d0d0d0;
-        background-color: #fff;
-    }
-    .menu_preview_area .sub_pre_menu_list li:first-child {
-        border-top: 1px solid #d0d0d0;
-    }
-    .menu_preview_area .sub_pre_menu_list li {
-        line-height: 44px;
-        border: 1px solid transparent;
-        margin: 0 -1px -1px;
-    }
-    .sub_pre_menu_list li a {
-        padding: 0 .5em;
-    }
-    .pre_menu_item a {
-        display: block;
-        width: auto;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        word-wrap: normal;
-        color: #616161;
-        text-decoration: none;
-    }
-    .menu_preview_area .sub_pre_menu_list li:first-child .sub_pre_menu_inner {
-        border-top-width: 0;
-    }
-    .menu_preview_area .sub_pre_menu_inner {
-        display: block;
-        border-top: 1px solid #e7e7eb;
-        width: auto;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        word-wrap: normal;
-        cursor: pointer;
-    }
-    .sub_pre_menu_box .arrow_out {
-        bottom: -6px;
-        display: inline-block;
-        width: 0;
-        height: 0;
-        border-width: 6px;
-        border-style: dashed;
-        border-color: transparent;
-        border-bottom-width: 0;
-        border-top-color: #d0d0d0;
-        border-top-style: solid;
-    }
-    .sub_pre_menu_box .arrow {
-        position: absolute;
-        left: 50%;
-        margin-left: -6px;
-    }
-    .sub_pre_menu_box .arrow_in {
-        bottom: -5px;
-        display: inline-block;
-        width: 0;
-        height: 0;
-        border-width: 6px;
-        border-style: dashed;
-        border-color: transparent;
-        border-bottom-width: 0;
-        border-top-color: #fafafa;
-        border-top-style: solid;
-    }
-    .menu_preview_area .sub_pre_menu_list li.current {
-        background-color: #fff;
-        border: 1px solid #44b549;
-        position: relative;
-        z-index: 1;
-        line-height: 45px;
-    }
-
-
-</style>
-
 <script>
 
     export default{
@@ -451,8 +228,8 @@
                 publicName: '永川优生活',
                 menu:{
                     "button":[
-                        {"type":"click","name":"歌手简介","key":"V1001_TODAY_SINGER","url":"","sub_button":[]},
-                        {"name":"菜单","sub_button":[
+                        {"type":"click","name":"优生活","key":"V1001_TODAY_SINGER","url":"","sub_button":[]},
+                        {"name":"社区","sub_button":[
                             {"type":"view","name":"搜索","key":"","url":"http://www.soso.com/","sub_button":[]},
                             {"type":"view","name":"视频","key":"","url":"http://v.qq.com/","sub_button":[]},
                             {"type":"click","name":"赞一下我们","key":"V1001_GOOD","url":"","sub_button":[]}
@@ -490,7 +267,7 @@
                 }
             },
             //添加一级菜单
-            menu_add() {
+            menuAdd() {
                 if(this.menu.button.length < 3){
                     this.activeMenuItemIndex = '';
                     this.activeMenuItemName = '';
@@ -507,7 +284,7 @@
                 }
             },
             //添加二级菜单
-            menu_item_add() {
+            menuItemAdd() {
                 if(this.menu.button[this.activeMenuIndex].sub_button.length < 5){
                     this.menu.button[this.activeMenuIndex].sub_button.push({"type":"click","name":"子菜单名称","key":"","url":"","sub_button":[]});
                     this.activeMenuItemIndex = this.menu.button[this.activeMenuIndex].sub_button.length-1;
@@ -520,7 +297,7 @@
                 }
             },
             //当前菜单
-            menu_selected(name,index) {
+            menuSelected(name,index) {
                 //判断是否有下级子菜单
             	console.log(this.menu.button)
             	console.log(123)
@@ -541,7 +318,7 @@
                 }
             },
             //当前子菜单选中
-            menu_item_selected(name,index) {
+            menuItemSelected(name,index) {
                 this.showDelBtnType = 2;
                 this.activeMenuItemName = name;
                 this.activeMenuItemIndex = index;
@@ -556,7 +333,7 @@
                 }
             },
             //菜单删除
-            menu_del() {
+            menuDel() {
                 if(this.menu.button.length <= 3 && this.menu.button.length >0){
 
                     if((this.activeMenuIndex !== 0) && (this.activeMenuIndex == this.menu.button.length-1)){
@@ -577,8 +354,29 @@
                     }
                 }
             },
+            menuItemDel() {
+                if(this.menu.button[this.activeMenuIndex].sub_button.length <=5 && this.menu.button[this.activeMenuIndex].sub_button.length>0 ){
+                    if(this.activeMenuItemIndex == this.menu.button[this.activeMenuIndex].sub_button.length-1){
+                        this.menu.button[this.activeMenuIndex].sub_button.splice(this.activeMenuItemIndex,1);
+                        this.activeMenuItemIndex -= 1;
+                    }else if(this.activeMenuItemIndex == 0){
+                        this.menu.button[this.activeMenuIndex].sub_button.splice(this.activeMenuItemIndex,1);
+                        this.activeMenuItemIndex = 0;
+                    }else{
+                        this.menu.button[this.activeMenuIndex].sub_button.splice(this.activeMenuItemIndex,1);
+                    }
+
+                    if(this.menu.button[this.activeMenuIndex].sub_button.length == 0){
+                        this.activeMenuIndex = '';
+                        this.activeMenuName = '';
+                        this.activeMenuItemIndex = '';
+                        this.activeMenuItemName = '';
+                    }
+                    this.showMenuContentType = this.activeMenuBtnType();
+                }
+            },
             //菜单内容选项
-            radio_label_selected(val) {
+            radioLabelSelected(val) {
                 if(val == 1){
                     this.setType('click');
                 }else if(val == 2){
