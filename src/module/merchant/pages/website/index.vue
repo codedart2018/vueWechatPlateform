1361c7e4ffadd18208b8c04a407d68d99dd3dea2
myxingke <myxingke@126.com>
2017-06-26 22:20:14 +0800

    微网站 百度地址组件

--- src/module/merchant/pages/website/index.vue
+++ src/module/merchant/pages/website/index.vue
@@ -4,35 +4,42 @@
             <Tabs>
                 <Tab-pane label="基础设置" icon="android-apps">
                     <Col span="12">
-                        <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="100">
-                            <Form-item label="网站名称" prop="name">
-                                <Input v-model="formValidate.name" placeholder="请输入姓名"></Input>
-                            </Form-item>
-                            <Form-item label="网站LOGO" prop="mail">
-                                <Upload action="//jsonplaceholder.typicode.com/posts/" success="uploadSuccess">
-                                    <Button type="ghost" icon="ios-cloud-upload-outline">上传文件</Button>
-                                </Upload>
-                            </Form-item>
-                            <Form-item label="联系电话" prop="mail">
-                                <Input v-model="formValidate.mail" placeholder="请输入邮箱"></Input>
-                            </Form-item>
-                            <Form-item label="电子邮箱" prop="mail">
-                                <Input v-model="formValidate.mail" placeholder="请输入邮箱"></Input>
-                            </Form-item>
-                            <Form-item label="联系地址" prop="mail">
-                                <Input v-model="formValidate.mail" placeholder="请输入邮箱"></Input>
-                            </Form-item>
-                            <Form-item label="地图坐标" prop="mail">
-                                <Input v-model="formValidate.mail" placeholder="请输入邮箱"></Input>
-                            </Form-item>
-                            <Form-item label="网站介绍" prop="desc">
-                                <Input v-model="formValidate.desc" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="请输入..."></Input>
-                            </Form-item>
-                            <Form-item>
-                                <Button type="primary" @click="handleSubmit('formValidate')">提交</Button>
-                                <Button type="ghost" @click="handleReset('formValidate')" style="margin-left: 8px">重置</Button>
-                            </Form-item>
-                        </Form>
+                    <Form ref="formField" :model="formField" :rules="ruleValidate" :label-width="100">
+                        <Form-item label="网站名称" prop="title">
+                            <Input v-model="formField.title" placeholder="请填写网站名称"></Input>
+                        </Form-item>
+                        <Form-item label="网站LOGO" prop="logo">
+                            <Upload action="//jsonplaceholder.typicode.com/posts/" success="uploadSuccess">
+                                <Button type="ghost" icon="ios-cloud-upload-outline">上传文件</Button>
+                            </Upload>
+                        </Form-item>
+                        <Form-item label="联系电话" prop="tel">
+                            <Input v-model="formField.tel" placeholder="请填写联系电话"></Input>
+                        </Form-item>
+                        <Form-item label="联系手机" prop="mobile">
+                            <Input v-model="formField.mobile" placeholder="请填写联系手机"></Input>
+                        </Form-item>
+                        <Form-item label="电子邮箱" prop="mail">
+                            <Input v-model="formField.mail" placeholder="请填写电子邮箱"></Input>
+                        </Form-item>
+                        <Form-item label="联系地址" prop="address">
+                            <Input v-model="formField.address" placeholder="请填写联系地址"></Input>
+                        </Form-item>
+                        <Form-item label="邮政编码" prop="zip">
+                            <Input v-model="formField.zip" placeholder="请填写邮政编码"></Input>
+                        </Form-item>
+                        <Form-item label="地图坐标" prop="location">
+                            <Button type="ghost" icon="location">点击获取</Button>
+                        </Form-item>
+                        <Form-item label="网站介绍" prop="desc">
+                            <Input v-model="formField.desc" type="textarea" :autosize="{minRows: 2,maxRows: 5}"
+                                   placeholder="请输入..."></Input>
+                        </Form-item>
+                        <Form-item>
+                            <Button type="primary" @click="handleSubmit('formField')">提交</Button>
+                            <Button type="ghost" @click="handleReset('formField')" style="margin-left: 8px">重置</Button>
+                        </Form-item>
+                    </Form>
                     </Col>
                 </Tab-pane>
                 <Tab-pane label="模板风格" icon="social-windows">模板风格</Tab-pane>
@@ -42,51 +49,38 @@
     </div>
 </template>
 <style>
-    body{
-        background-color:#ff0000;
-    }
+
 </style>
 <script>
     export default{
         data () {
             return {
-                formValidate: {
-                    name: '',
+                formField: {
+                    title: '',
+                    logo: '',
+                    tel: '',
+                    mobile: '',
                     mail: '',
-                    city: '',
-                    gender: '',
-                    interest: [],
-                    date: '',
-                    time: '',
+                    zip: '',
+                    location: '',
+                    address: '',
                     desc: ''
                 },
                 ruleValidate: {
-                    name: [
-                        { required: true, message: '姓名不能为空', trigger: 'blur' }
+                    title: [
+                        {required: true, message: '网站名称不能为空', trigger: 'blur'},
+                        {type: 'string', min: 2, message: '网站不能小于2个字符', trigger: 'blur'},
+                        {type: 'string', max: 15, message: '联系地址不能大于15个字符', trigger: 'blur'}
                     ],
                     mail: [
-                        { required: true, message: '邮箱不能为空', trigger: 'blur' },
-                        { type: 'email', message: '邮箱格式不正确', trigger: 'blur' }
+                        {type: 'email', message: '邮箱格式不正确', trigger: 'blur'}
                     ],
-                    city: [
-                        { required: true, message: '请选择城市', trigger: 'change' }
-                    ],
-                    gender: [
-                        { required: true, message: '请选择性别', trigger: 'change' }
-                    ],
-                    interest: [
-                        { required: true, type: 'array', min: 1, message: '至少选择一个爱好', trigger: 'change' },
-                        { type: 'array', max: 2, message: '最多选择两个爱好', trigger: 'change' }
-                    ],
-                    date: [
-                        { required: true, type: 'date', message: '请选择日期', trigger: 'change' }
-                    ],
-                    time: [
-                        { required: true, type: 'date', message: '请选择时间', trigger: 'change' }
+                    address: [
+                        {type: 'string', min: 6, message: '联系地址不能小于6个字符', trigger: 'blur'},
+                        {type: 'string', max: 50, message: '联系地址不能大于50个字符', trigger: 'blur'}
                     ],
                     desc: [
-                        { required: true, message: '请输入个人介绍', trigger: 'blur' },
-                        { type: 'string', min: 20, message: '介绍不能少于20字', trigger: 'blur' }
+                        {type: 'string', min: 20, message: '介绍不能少于20字', trigger: 'blur'}
                     ]
                 }
             }
@@ -105,7 +99,6 @@
                 this.$refs[name].resetFields();
             }
         },
-        components:{
-        }
+        components: {}
     }
 </script>
