d96719d401c2968df125ecb36a14884558b8a5e6
myxingke <myxingke@126.com>
2017-06-29 16:37:23 +0800

    微站模板

---
+++ src/module/manage/pages/website/template.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div>微站模板</div>
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+        }
+    }
+</script>
