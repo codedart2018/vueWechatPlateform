b8419e83bc7273f74d497d729b52e8d254e67a15
myxingke <myxingke@126.com>
2017-07-06 09:27:08 +0800

    绑定授权

--- src/libs/api.js
+++ src/libs/api.js
@@ -4,6 +4,6 @@
 const Api = window.Api || {}
 /** 主请求域名**/
 Api.domain = process.env.NODE_ENV == 'production' ? '' : ''
-Api.auth_call_back = process.env.NODE_ENV == 'production' ? 'http://api.daimatu.cn' : 'http://xingke.iask.in'
+Api.auth_call_back = process.env.NODE_ENV == 'production' ? 'http://www.daimatu.cn' : 'http://www.hphp.cn'
 
 export default Api
